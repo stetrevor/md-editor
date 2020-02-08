9ea4194b8cdf5bcb24b284ab81c8d73984dda2f3
Trevor Wong <stephen.trevor.wong@outlook.com>
2020-02-08 20:24:55 +0800

    Change deploy to Firebase

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,30 @@
 # abort on errors
 set -e
 
+## Deploy to Firebase
+
+yarn build
+firebase deploy
+
+## Deploy to Github
 # build
-npm run build
+# npm run build
 
 # navigate into the build output directory
-cd dist
+# cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add -A
+# git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f https://github.com/stetrevor/md-editor.git master:gh-pages
+# git push -f https://github.com/stetrevor/md-editor.git master:gh-pages
 
 cd -
