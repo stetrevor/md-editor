17a067f3ec435ef17258835d77417014f37a062b
Trevor Wong <stephen.trevor.wong@outlook.com>
2020-02-13 12:40:28 +0800

    Update GenerateSW dev config

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,30 @@ const DEV = process.env.NODE_ENV === "development";
 
 const devSWPlugin = new GenerateSW({
   clientsClaim: true,
-  skipWaiting: true
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "google-fonts-stylesheets"
+      }
+    },
+    {
+      urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "google-fonts-webfonts",
+        cacheableResponse: {
+          statuses: [0, 200]
+        },
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60 * 24 * 365
+        }
+      }
+    }
+  ]
 });
 
 module.exports = {
